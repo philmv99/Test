be0df75ad33f49e1d7aeb45491b75583e4511535
Philmv99 <philipmv99@gmail.com>
2023-09-21 13:07:29 -0400

    test2

--- bli/blyat.go
+++ bli/blyat.go
@@ -4,28 +4,48 @@ import (
 	"fmt"
 )
 
-func main() {
-	var hourlyRate float64
-	fmt.Print("Enter hourly salary rate: $")
-	fmt.Scanln(&hourlyRate)
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
 
-	totalHours := 0.0
-
-	for i := 0; i < 7; i++ {
-		fmt.Printf("Enter hours worked on day %d (hh:mm): ", i+1)
-		var hours, minutes int
-		_, err := fmt.Scanf("%d:%d", &hours, &minutes)
-
-		if err != nil || hours < 0 || minutes < 0 || minutes >= 60 {
-			fmt.Println("Invalid input. Please use the format hh:mm.")
-			return
-		}
-
-		totalHours += float64(hours) + float64(minutes)/60.0
+func findLowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
 	}
 
-	totalEarnings := totalHours * hourlyRate
+	if root.Val == p.Val || root.Val == q.Val {
+		return root
+	}
 
-	fmt.Printf("\nTotal hours worked this week: %.2f\n", totalHours)
-	fmt.Printf("Total earnings for the week: $%.2f\n", totalEarnings)
+	left := findLowestCommonAncestor(root.Left, p, q)
+	right := findLowestCommonAncestor(root.Right, p, q)
+
+	if left != nil && right != nil {
+		return root
+	} else if left != nil {
+		return left
+	} else {
+		return right
+	}
+}
+
+func main() {
+	// Create a sample binary tree
+	root := &TreeNode{Val: 3}
+	root.Left = &TreeNode{Val: 5}
+	root.Right = &TreeNode{Val: 1}
+	root.Left.Left = &TreeNode{Val: 6}
+	root.Left.Right = &TreeNode{Val: 2}
+	root.Right.Left = &TreeNode{Val: 0}
+	root.Right.Right = &TreeNode{Val: 8}
+	root.Left.Right.Left = &TreeNode{Val: 7}
+	root.Left.Right.Right = &TreeNode{Val: 4}
+
+	p := &TreeNode{Val: 6}
+	q := &TreeNode{Val: 4}
+
+	lca := findLowestCommonAncestor(root, p, q)
+	fmt.Printf("Lowest Common Ancestor of %d and %d is %d\n", p.Val, q.Val, lca.Val)
 }
